73714d7095d6e53f66f75dc5a96d9c8c757ebc0d
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-06-21 17:59:46 +0100

    Add promise example with event loop demonstration

---
+++ Chaining/task.js
@@ -0,0 +1,12 @@
+const promise = new Promise((resolve, reject) => {
+  console.log("Promise callback");
+  resolve();
+}).then((result) => {
+  console.log("Promise callback (.then)");
+});
+
+setTimeout(() => {
+  console.log("event-loop cycle: Promise (fulfilled)", promise);
+}, 0);
+
+console.log("Promise (pending)", promise);
